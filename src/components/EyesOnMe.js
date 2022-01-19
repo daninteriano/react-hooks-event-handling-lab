839cb589d024d5a71d764466a0fe07c2e6c91e14
Daniel Interiano <dannyboyint@gmail.com>
2022-01-19 12:07:43 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe(){
+    function focusFunction(event){
+        console.log("Good!");
+    }
+    function blurFunction(event){
+        console.log("Hey! Eyes on me!");
+    }
+    return (
+        <button onFocus={focusFunction} onBlur={blurFunction}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
 // Code Keypad Component Here
+import React from "react";
+
+function Keypad(){
+    function inputChange(event){
+        event.preventDefault();
+        console.log(`Entering password...`)
+    }
+    return (
+        <input type="password" onChange={inputChange}></input>
+    )
+}
+
+export default Keypad;
